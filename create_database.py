9a7dc2ac18583c03a2add4e7cde3a1094db01f56
Vanderlei Matos Andre <vmandre@gmail.com>
2025-04-28 23:45:42 -0300

    Remove database from github and add it to .gitignore

--- create_database.py
+++ create_database.py
@@ -4,4 +4,4 @@ from fakepinterest.models import *
 with app.app_context():
     database.create_all()
 
-# para criar o DB, basta executa este arquivo create_database.py
+# para criar o DB, basta executar este arquivo: python3 create_database.py
